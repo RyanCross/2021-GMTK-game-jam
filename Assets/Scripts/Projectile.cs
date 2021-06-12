b97304bc1a881093766cde12005471976c4ac941
cole-c <cole.e.cummings@gmail.com>
2021-06-11 20:51:35 -0500

    check title

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public GameObject hitEffect; //put some explosion or some shit here.
+    public float timeToLive = 2.0f;
+
+    private void Start()
+    {
+        Destroy(gameObject, 2.0f);
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
