ee2e5ccc8bb9d793cb10671c7885eb6c2b7eef64
Ryan Cross <ryan.cross.home@gmail.com>
2021-06-12 17:46:01 -0500

    modularize shooting parameters based on form, add actually form swapping rotation (#7)

--- Assets/Scripts/ShootingController.cs
+++ Assets/Scripts/ShootingController.cs
@@ -10,25 +10,32 @@ public class ShootingController : MonoBehaviour
     public float bulletForce = 20f;
     public PlayerController player;
 
+    public float bulletSpeed;
+
+    public float fireRate = 2f;
+    private float timeTillNextShot;
+
+
     void Start()
     {
         Debug.Log(player.currentForm);
+        timeTillNextShot = .01f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        timeTillNextShot -= Time.deltaTime;
+        if (Input.GetButtonDown("Fire1") && timeTillNextShot <= 0)
         {
             Shoot();
+            timeTillNextShot = player.currentForm.fireRate;
             Debug.Log(player.currentForm);
         }          
     }
 
     void Shoot()
     {
-       GameObject bullet = Instantiate(currentProjectilePrefab, firePoint.position, firePoint.rotation);
-       Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        player.currentForm.shootingStyle.GetComponent<ShootingStyle>().Shoot(firePoint, player.currentForm.fireRate, player.currentForm.formProjectile, player.currentForm.projectileForce);
     }
 }
