b95086ce4b5ea1a9ea06e0a8265a915bea481b67
Ryan Cross <ryan.cross.home@gmail.com>
2021-06-12 23:49:27 -0500

    Create main level, lots of little fixes

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -16,15 +16,15 @@ public class Projectile : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D hitInfo)
     {
        IKillable target = hitInfo.GetComponent<IKillable>();
-        if (target != null) // target is something that can take dmg
+        if (target != null && hitInfo.tag != "Player") // target is something that can take dmg, but is not the player
         {
             target.TakeDamage(1);
             Debug.Log("damage taken");
         }
         
-        if(hitInfo.tag != "Projectile")
+        if(hitInfo.tag != "Projectile" && hitInfo.tag != "Player") 
         {
-            Destroy(gameObject);
+                Destroy(gameObject);   
         }
         
     }
