8d7e669b392f124089f80ba5f893b3cedc254fd0
Ryan Cross <ryan.cross.home@gmail.com>
2021-06-13 09:24:38 -0500

    Update aim and rig sprite animations for running (#13)
    * Update aiming to use little reticle, rig the running for the player
    
    * basic mainmenu

---
+++ Assets/Scripts/AimController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimController : MonoBehaviour
+{
+    public Transform aimTransform;
+    private Vector3 lookAtPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = GetMouseWorldPosition();
+
+        Vector3 aimDirection = (mousePosition - aimTransform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f; // not sure why the 90 * offset is necessary but it is.
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    // Get Mouse Position in World with Z = 0f
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
